a7671fa74bc4e26e464ea54e2d582469785e6201
Bauti4ci <bauti.forci@gmail.com>
2026-05-17 17:56:39 -0300

    feat: initial structure for frontend with Flask y Jinja2

---
+++ frontend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('dashboard/index.html', titulo="Sistema de Gestión Educativa")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
