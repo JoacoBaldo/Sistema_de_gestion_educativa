ba75c3ff7a3dea8269ce48ea0b52a49e913be732
Bauti4ci <bauti.forci@gmail.com>
2026-05-23 11:23:06 -0300

    Implementation of login views and changes in structure for fute implementations

--- frontend/app.py
+++ frontend/app.py
@@ -2,13 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
 
-@app.route("/")
-def index():
-    return render_template(
-        "dashboard/index.html", titulo="Sistema de Gestión Educativa"
-    )
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True, port=5001)
