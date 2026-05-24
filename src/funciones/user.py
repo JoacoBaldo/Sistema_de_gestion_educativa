55fb3ff6b2a596df17c6ec67b71fe8d837b5f5a0
Pedro Saliani <pedrosaliani@gmail.com>
2026-05-24 17:28:46 -0300

    Cambio nombre de la funcion src/funciones/user/change_password.

--- src/funciones/user.py
+++ src/funciones/user.py
@@ -1,5 +1,10 @@
 from src.db.user import create_User_db, email_exists, change_password_db
-from src.funciones.errores import EMAIL_NO_EXISTE, EMAIL_NO_VALIDO, EMAIL_YA_EXISTE, CONTRASENA_DEBIL
+from src.funciones.errores import (
+    EMAIL_NO_EXISTE,
+    EMAIL_NO_VALIDO,
+    EMAIL_YA_EXISTE,
+    CONTRASENA_DEBIL,
+)
 
 
 def change_password_mail(email: str) -> dict:
@@ -7,11 +12,13 @@ def change_password_mail(email: str) -> dict:
         return EMAIL_NO_EXISTE
     # funcion para enviar mail con link para cambiar contraseña
 
+    return {
+        "message": "Enviamos el mail con las instrucciones para cambiar la contraseña.",
+        "status_code": 200,
+    }
 
-    return {"message": "Enviamos el mail con las instrucciones para cambiar la contraseña.", "status_code": 200}
 
-
-def change_password_db(new_password_data: dict) -> dict:
+def change_password(new_password_data: dict) -> dict:
     if not email_exists(new_password_data["email"]):
         return EMAIL_NO_EXISTE
     return change_password_db(new_password_data)
@@ -22,7 +29,7 @@ def create_user(user: dict) -> dict:
         return EMAIL_YA_EXISTE
     if "@fi.uba.ar" not in user["email"]:
         return EMAIL_NO_VALIDO
-    if len(user["password"]) < 6: 
+    if len(user["password"]) < 6:
         return CONTRASENA_DEBIL
-     
+
     return create_User_db(user)
