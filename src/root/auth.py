d175389b1604f7f6f1b02919f9af504537c12c6f
Ezequiel Di Sabatino <jdisabatino@fi.uba.ar>
2026-05-26 17:44:14 -0300

    Agrega funciones para actualizar la contrasenia de un usuario. Ademas añade auth.bp al main

---
+++ src/root/auth.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify, request
+from werkzeug.security import generate_password_hash
+from src.funciones.auth import datos_completos
+
+auth_bp = Blueprint("auth", __name__)
+
+
+@app.route('/api/auth/actualizar-contrasenia', methods=['POST'])
+def actualizar_contrasenia():
+    
+    token, nueva_contrasenia, error = datos_completos()
+    
+    if error:
+        return jsonify({"error": error["error"]}), error["status"]
+
+    token_activo, error = buscar_token(token)
+
+    if error:
+        return jsonify({"error": error["error"]}), error["status"]
+
+    id_usuario, error = usuario_existe(token_activo.usuario_id)
+
+    if error:
+        return jsonify({"error": error["error"]}), error["status"]
+
+    hash_generado = generate_password_hash(nueva_contrasenia)
+
+    resultado = actualizar_contrasenia(id_usuario, hash_generado)
+
+    return jsnify{resultado}, 200
