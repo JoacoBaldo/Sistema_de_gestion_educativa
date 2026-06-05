06dd5fafab41fec5e441ab2608dab6d31ed1abf9
Pedro Saliani <pedrosaliani@gmail.com>
2026-06-05 19:47:36 -0300

    Agrego endpoint para sumar una inasistencia a todos los alumnos de una clase.

--- src/core/usecase/users/users.py
+++ src/core/usecase/users/users.py
@@ -3,32 +3,41 @@ import bcrypt
 from src.repositories.users.create_user import create_UserRepository
 from src.core.contracts.response.users.users import create_user_reponse
 
+
 def hash_pasword(password: str) -> str:
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password.decode("utf-8")
+
 
 def validate_user_data(user: dict) -> dict:
     if not user.get("username") or not user.get("email") or not user.get("password"):
-        return {"error": "Username, email, and password are required", "status_code": 400}
+        return {
+            "error": "Username, email, and password are required",
+            "status_code": 400,
+        }
     if len(user["password"]) < 6:
-        return {"error": "Password must be at least 6 characters long", "status_code": 400}
+        return {
+            "error": "Password must be at least 6 characters long",
+            "status_code": 400,
+        }
     if "@" not in user["email"]:
         return {"error": "Invalid email format", "status_code": 400}
     return None
+
 
 def execute(user_requests: User) -> dict:
     validation_result = validate_user_data(user_requests)
     if validation_result is not None:
         return validation_result
-    
+
     hashed_password = hash_pasword(user_requests["password"])
     user_requests["password"] = hashed_password
 
     repository_result = create_UserRepository(user_requests)
     if repository_result:
         return repository_result
-    
+
     user_reponse = user_requests.copy()
     del user_reponse["password"]
     return create_user_reponse(user_reponse)
